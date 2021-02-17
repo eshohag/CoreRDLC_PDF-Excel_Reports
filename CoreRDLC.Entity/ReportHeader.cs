0d1a2c404b6058594c3a793122f3557c4ddfad8f
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-02-17 14:36:37 +0600

    Update Projects

---
+++ CoreRDLC.Entity/ReportHeader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreRDLC.Entity
+{
+   public class ReportHeader
+    {
+        public string Logo { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ CoreRDLC.Extension/ExtensionMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Reflection;
+
+namespace CoreRDLC.Extension
+{
+    public static class ExtensionMethod
+    {
+        public static DataTable ToDataTable<TSource>(this TSource data)
+        {
+            var dataTable = new DataTable(typeof(TSource).Name);
+            var props = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var prop in props)
+            {
+                dataTable.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ??
+                                                 prop.PropertyType);
+            }
+
+            var values = new object[props.Length];
+            for (int i = 0; i < props.Length; i++)
+            {
+                values[i] = props[i].GetValue(data, null);
+            }
+            dataTable.Rows.Add(values);
+
+            return dataTable;
+        }
+    }
+}
